79c95e04e7d58d22b3e958a006eba75cc4458dc2
Laptop-Salad <awallis1609@gmail.com>
2024-09-07 17:15:47 +0100

    ui(project): project tab improvements

--- resources/views/components/project/tab.blade.php
+++ resources/views/components/project/tab.blade.php
@@ -1,8 +1,13 @@
+@props(['active'])
+
 <button
-    {{
-    $attributes->merge([
-        'class' => 'py-2 rounded-t border-x border-t border-ws-green hover:bg-ws-green hover:text-stone-900 text-lg px-6'
-    ])}}
+    {{$attributes}}
+
+    @class([
+        'text-stone-900 bg-ws-green pt-4' => $active,
+        'hover:pt-4 hover:mt-0 mt-4' => !$active,
+        'py-2 rounded-t border-x border-t border-ws-green hover:bg-ws-green hover:text-stone-900 text-lg px-6 mt-4',
+    ])
 >
     {{$slot}}
 </button>
--- resources/views/livewire/show-project.blade.php
+++ resources/views/livewire/show-project.blade.php
@@ -4,20 +4,25 @@
        <x-gen.btn wire:click="save">Save</x-gen.btn>
    </div>
 
-    <div class="border-b border-ws-green pt-4">
+    <div class="pt-4 mx-2">
         @foreach($this->contents as $page => $content)
-            <x-project.tab wire:click="$set('current_page', '{{$page}}')">{{ucwords(str_replace('_', ' ', $page))}}</x-project.tab>
+            <x-project.tab
+                :active="$this->current_page === $page"
+                wire:click="$set('current_page', '{{$page}}')"
+            >
+                {{ucwords(str_replace('_', ' ', $page))}}
+            </x-project.tab>
         @endforeach
     </div>
 
-    <div class="grid grid-cols-[1fr_6fr] border-x border-b border-ws-green">
+    <div class="grid grid-cols-[1fr_6fr] border border-ws-green rounded-md">
         <div class="px-2 space-y-2 text-xl py-5 h-[100vh]">
             <x-gen.btn wire:click="addPage">New Page</x-gen.btn>
             <x-gen.btn>T</x-gen.btn>
         </div>
 
         @isset($this->current_page)
-            <div class="bg-white text-black h-[100vh]">
+            <div class="bg-white text-black h-[100vh] m-5">
                 @foreach($this->children as $child)
                     {!! $child !!}
                 @endforeach
